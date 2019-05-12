f3c7b911f8ee71c8a4cd1a1c49def26561eb491a
THelsby <thomas-helsby@hotmail.co.uk>
2019-05-12 03:06:55 +0100

    Updates

---
+++ precache-manifest.490ad9d4b52218b7cd05d3b1b585f631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457b1e841adfa748baf4ca78b1cb2260",
+    "url": "/React-ReduxPractice/index.html"
+  },
+  {
+    "revision": "3d9719e0566b18594914",
+    "url": "/React-ReduxPractice/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c2b5576e0beff22fe346",
+    "url": "/React-ReduxPractice/static/css/main.2321a727.chunk.css"
+  },
+  {
+    "revision": "3d9719e0566b18594914",
+    "url": "/React-ReduxPractice/static/js/2.2b1267ea.chunk.js"
+  },
+  {
+    "revision": "c2b5576e0beff22fe346",
+    "url": "/React-ReduxPractice/static/js/main.d94ad326.chunk.js"
+  },
+  {
+    "revision": "1e399e8fbbccf97550a3",
+    "url": "/React-ReduxPractice/static/js/runtime~main.19b8770c.js"
+  }
+]);
